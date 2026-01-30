0ebc54b96f35043ba8d460936ab20ebba4ace70c
Murillo Lécio <leciopmurillo@gmail.com>
2026-01-30 18:03:51 -0300

    Sending and Retrieve messages

--- app/src/main/java/com/example/chatapp/adaptors/MessagesAdaptor.kt
+++ app/src/main/java/com/example/chatapp/adaptors/MessagesAdaptor.kt
@@ -1,5 +1,3 @@
-package com.example.chatapp.adaptors
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -10,59 +8,42 @@ import com.example.chatapp.R
 import com.example.chatapp.model.ChatMessage
 import com.google.firebase.auth.FirebaseAuth
 import com.mikhaellopez.circularimageview.CircularImageView
-import com.squareup.picasso.Picasso
 
 class MessagesAdaptor(
     private val context: Context,
     private val messages: MutableList<ChatMessage>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private val RECEIVER_TYPE_HOLDER = 1
     private val SENDER_TYPE_HOLDER = 2
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return if (viewType == RECEIVER_TYPE_HOLDER) {
             MeViewHolder(
-                LayoutInflater.from(context).inflate(
-                    R.layout.me,
-                    parent,
-                    false
-                )
+                LayoutInflater.from(context).inflate(R.layout.me, parent, false)
             )
         } else {
             SenderViewHolder(
-                LayoutInflater.from(context).inflate(
-                    R.layout.sender,
-                    parent,
-                    false
-                )
+                LayoutInflater.from(context).inflate(R.layout.sender, parent, false)
             )
         }
     }
 
-    override fun onBindViewHolder(
-        holder: RecyclerView.ViewHolder,
-        position: Int
-    ) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val message = messages[position]
+
         if (holder is MeViewHolder) {
             holder.textViewMessage.text = message.message
-        } else if (holder is SenderViewHolder) {
-            holder.textViewSender.text = message.message
-            Picasso.get().load(R.drawable.ic_profile)
-                .into(holder.senderProfileImage)
-        } else {
-
         }
+        else if (holder is SenderViewHolder) {
+            holder.textViewSender.text = message.message
 
+            // ✅ imagem fixa do drawable
+            holder.senderProfileImage.setImageResource(R.drawable.ic_profile)
+        }
     }
 
-    override fun getItemCount(): Int {
-        return messages.size
-
-    }
+    override fun getItemCount(): Int = messages.size
 
     override fun getItemViewType(position: Int): Int {
         val message = messages[position]
@@ -79,7 +60,7 @@ class MessagesAdaptor(
 
     inner class SenderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textViewSender: TextView = view.findViewById(R.id.sender_text_view)
-        val senderProfileImage: CircularImageView = view.findViewById(R.id.sender_profile_image)
+        val senderProfileImage: CircularImageView =
+            view.findViewById(R.id.sender_profile_image)
     }
-
 }
--- app/src/main/java/com/example/chatapp/ui/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/ui/ChatActivity.kt
@@ -1,13 +1,34 @@
 package com.example.chatapp.ui
 
+import MessagesAdaptor
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapp.R
+import com.example.chatapp.model.ChatMessage
+import com.example.chatapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+    private val usersRef = db.collection("users_collection")
+    private val messagesRef: CollectionReference = db.collection("messages_collection")
+    private lateinit var sendButton: Button
+    private lateinit var editTextMessage: EditText
+    private lateinit var messagesAdapator: MessagesAdaptor
+    private lateinit var messagesRecyclerView: RecyclerView
+    private lateinit var messages: MutableList<ChatMessage>
+    private lateinit var currentUser: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +39,79 @@ class ChatActivity : AppCompatActivity() {
             insets
         }
 
+        messagesRecyclerView = findViewById(R.id.message_recycler_view)
+        sendButton = findViewById(R.id.send_message_button)
+        editTextMessage = findViewById(R.id.input_message)
 
+        initRecyclerView()
+        getCurrentUser()
+
+        sendButton.setOnClickListener {
+            insertMessage()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        messagesRef.addSnapshotListener { snapshots, error ->
+            error?.let {
+                return@addSnapshotListener
+            }
+
+            snapshots?.let {
+                for (dc in it.documentChanges) {
+                    val oldIndex = dc.oldIndex
+                    val newIndex = dc.newIndex
+
+                    when (dc.type) {
+                        DocumentChange.Type.ADDED -> {
+                            val snapshot = dc.document
+                            val message = snapshot.toObject(ChatMessage::class.java)
+                            messages.add(message)
+                            messagesAdapator.notifyItemInserted(newIndex)
+                        }
+
+                        DocumentChange.Type.REMOVED -> {
+
+                        }
+
+                        DocumentChange.Type.MODIFIED -> {
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
+        messages = mutableListOf()
+        messagesAdapator = MessagesAdaptor(this, messages)
+        messagesRecyclerView.adapter = messagesAdapator
+        messagesRecyclerView.layoutManager = LinearLayoutManager(this)
+        messagesRecyclerView.setHasFixedSize(true)
+    }
+
+    private fun getCurrentUser() {
+        usersRef.whereEqualTo("uid", FirebaseAuth.getInstance().currentUser?.uid)
+            .get()
+            .addOnSuccessListener {
+                for (snapshot in it) {
+                    currentUser = snapshot.toObject(User::class.java)
+                }
+            }
+    }
+
+    private fun insertMessage() {
+        val message = editTextMessage.text.toString().trim()
+
+        if ( message.isNotEmpty()) {
+            messagesRef.document()
+                .set(ChatMessage(currentUser, message))
+        }
     }
 }
